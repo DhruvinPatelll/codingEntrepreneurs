ba5eca95862d33aaa9d591f30b57f4c79403310a
DhruvinPatelll <here.dhruvin@gmail.com>
2024-06-05 17:09:14 +0530

    Using Function based views for create retrieve and list

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,6 +4,9 @@ from .models import Product
 from .serializers import ProductSerializer
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -45,3 +48,26 @@ class ProductListAPIView(generics.ListAPIView):
     # Product.objects.get(pk = pk)
 
 product_list_view = ProductListAPIView.as_view()
+
+@api_view(['GET','POST'])
+def product_alt_view(request,pk=None,*args,**kwargs):
+    method = request.method
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product,pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset,many=True).data
+        return Response(data)
+    if method == "POST":
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            description = serializer.validated_data.get('description') or None
+            if description is None:
+                description = title
+            price = serializer.validated_data.get('price')
+            serializer.save(description=description)
+            return Response(serializer.data)
+        return Response({"invalid":"Not appropriate data"},status=400)
--- py_client/not_found.py
+++ py_client/not_found.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://127.0.0.1:8005/api/products/1/"
+endpoint = "http://127.0.0.1:8005/api/products/1254847872153148798797431879/"
 
 get_response = requests.get(endpoint)
 
